9b1c6927b07941d742d99d8a9c1195a181c531d2
Mohamed Elhawary <mohamed.elhawary@thinktech-it.com>
2025-07-29 02:42:48 +0300

    Add project files.

---
+++ EraaSoftTasks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConditionTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter your mark(0-100): ");
+            double mark = Convert.ToDouble(Console.ReadLine());
+            if (mark > 90 && mark <= 100)
+            {
+                Console.WriteLine("\n your Grade: A ");
+            }
+            else if (mark > 80 && mark <= 89)
+            {
+                Console.WriteLine("\n your Grade: B ");
+            }
+            else if (mark > 70 && mark <= 79)
+            {
+                Console.WriteLine("\n your Grade: C ");
+            }
+            else if (mark > 60 && mark <= 69)
+            {
+                Console.WriteLine("\n your Grade: D ");
+            }
+            else if (mark < 60)
+            {
+                Console.WriteLine("\n your Grade: F ");
+            }
+            else
+            {
+
+                Console.WriteLine("\nPlease Enter the valid Mark");
+            }
+        }
+    }
+}
---
+++ LoopsTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+
+namespace LoopsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a number to print its multiplication table: \n");
+                double number = Convert.ToDouble(Console.ReadLine());
+                int i = 1;
+                while (i <= 12)
+                {
+                    Console.WriteLine($">> {number}*{i} = {number * i} ");
+
+                    i++;
+                }
+            }
+        }
+    }
+}
